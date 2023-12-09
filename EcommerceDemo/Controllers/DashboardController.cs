a6edb42abba030f811ee11c303838563c2f1b4a7
Gerardo Lanza <glanza007@gmail.com>
2023-12-09 11:53:58 -0600

    Dashboard

---
+++ EcommerceDemo/Controllers/DashboardController.cs
@@ -0,0 +1,33 @@
+﻿using EcommerceDemo.Enums;
+using EcommerceDemo.Models;
+using EcommerceDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceDemo.Controllers
+{
+    public class DashboardController : Controller
+    {
+        
+        private readonly EcommerceContext _context;
+        private readonly IServicioUsuario _servicioUsuario;
+
+        public DashboardController(EcommerceContext context, IServicioUsuario servicioUsuario)
+        {
+            _context = context;
+            _servicioUsuario = servicioUsuario;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.CantidadUsuarios = _context.Usuarios.Count();
+            ViewBag.CantidadProductos = _context.Productos.Count();
+            ViewBag.CantidadVentas = _context.Ventas.Where(o => o.Estado == Estado.Nuevo).Count();
+            ViewBag.CantidadVentasConfirmadas = _context.Ventas.Where(o => o.Estado == Estado.Confirmado).Count();
+
+            return View(await _context.Temporales
+                    .Include(u => u.Usuario)
+                    .Include(p => p.Producto).ToListAsync());
+        }
+    }
+}
---
+++ EcommerceDemo/Controllers/VentasController.cs
@@ -0,0 +1,85 @@
+﻿using EcommerceDemo.Models;
+using EcommerceDemo.Models.Entidades;
+using EcommerceDemo.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceDemo.Controllers
+{
+    public class VentasController : Controller
+    {
+        private readonly EcommerceContext _context;
+        private readonly IServicioVenta _servicioVenta;
+
+        public VentasController(EcommerceContext context, IServicioVenta servicioVenta)
+        {
+            _context = context;
+            _servicioVenta = servicioVenta;
+        }
+
+        //[Authorize(Roles = "Administrador")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Ventas
+                .Include(s => s.Usuario)
+                .Include(s => s.DetallesVenta)
+                .ThenInclude(sd => sd.Producto)
+                .ToListAsync());
+        }
+
+        //[Authorize(Roles = "Administrador")]
+        public async Task<IActionResult> Detalles(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Venta venta = await _context.Ventas
+                .Include(s => s.Usuario)
+                .Include(s => s.DetallesVenta)
+                .ThenInclude(sd => sd.Producto)                
+                .FirstOrDefaultAsync(s => s.Id == id);
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            return View(venta);
+        }
+
+        [Authorize(Roles = "Cliente")]
+        public async Task<IActionResult> MisCompras()
+        {
+            return View(await _context.Ventas
+                .Include(s => s.Usuario)
+                .Include(s => s.DetallesVenta)
+                .ThenInclude(sd => sd.Producto)
+                .Where(s => s.Usuario.Email == User.Identity.Name)
+                .ToListAsync());
+        }
+
+        [Authorize(Roles = "Cliente")]
+        public async Task<IActionResult> MisDetalles(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Venta venta = await _context.Ventas
+                .Include(s => s.Usuario)
+                .Include(s => s.DetallesVenta)
+                .ThenInclude(sd => sd.Producto)              
+                .FirstOrDefaultAsync(s => s.Id == id);
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            return View(venta);
+        }
+
+    }
+}
